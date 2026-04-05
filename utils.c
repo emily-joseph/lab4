6e24f42510eb3f8bc06f3c670b61b18719525460
emily-joseph <emilyjesu@gmail.com>
2026-04-05 04:18:10 -0500

    shortest path w/o print (should work..)

--- utils.c
+++ utils.c
@@ -29,6 +29,7 @@ int check_integrity(void) {
 
         if (q_dequeue(q, &curr, &id) == 0) {
             q_free(q);
+            free(q);
             return 0;
             //fail
         }
@@ -50,6 +51,7 @@ int check_integrity(void) {
             q_enqueue(q, curr->no, currId);
         }
     }
+    return 1;
 }
 
 /* ----------------------------------------------------------------
@@ -71,6 +73,149 @@ void find_shortest_path(const char *sol1, const char *sol2) {
         refresh();
         return;
     }
-    mvprintw(10, 2, "find_shortest_path not yet implemented.");
-    refresh();
+
+    if (sol1 == NULL || sol2 == NULL) {
+        mvprintw(10, 2, "Error: invalid solution text.");
+        refresh();
+        return;
+    }
+    
+    int nodeCount = count_nodes(g_root);
+    if (nodeCount <= 0) {
+        mvprintw(10, 2, "Error: empty knowledge base.");
+        refresh();
+        return;
+    }
+
+    typedef struct {
+        Node *node;
+        int32_t pId;          //will use -1 for root
+        int32_t branch;  //-1 root, 1 yes, 0 no
+    } PathNode;
+
+    PathNode *arr = calloc(nodeCount, sizeof(PathNode));
+    if (arr == NULL) {
+        mvprintw(10, 2, "Error: Memory issue.");
+        refresh();
+        return;
+    }
+
+    Queue* q = malloc(sizeof(Queue));
+    if (q == NULL) {
+        free(arr);
+        mvprintw(10, 2, "Error: Memory issue.");
+        refresh();
+        return;
+    }
+    q_init(q);
+    int32_t found1 = -1;
+    int32_t found2 = -1;
+    uint32_t nextId = 0;
+
+    arr[0].node = g_root;
+    arr[0].pId = -1;
+    arr[0].branch = -1;
+    q_enqueue(q, g_root, 0);
+
+    while (!q_empty(q) && (found1==-1 || found2==-1)) {
+        Node* curr;
+        int id;
+
+        if (!q_dequeue(q, &curr, &id)) {
+            q_free(q);
+            free(q);
+            free(arr);
+            mvprintw(10, 2, "Error: BFS failed.");
+            refresh();
+            return;
+        }
+
+        //trying to find the two solutions
+        if (curr->isQuestion == 0) { //solution!
+            if (strcmp(curr->text, sol1) == 0 && found1 == -1) {
+                found1 = id;
+            } 
+            if (strcmp(curr->text, sol2) == 0 && found2 == -1) {
+                found2 = id;
+            } 
+        }
+
+        //update queue to continue search
+        if (curr->yes != NULL) {
+            nextId++;
+            arr[nextId].node = curr->yes;
+            arr[nextId].pId = id;
+            arr[nextId].branch = 1;
+            q_enqueue(q, curr->yes, nextId);
+        }
+        if (curr->no != NULL) {
+            nextId++;
+            arr[nextId].node = curr->no;
+            arr[nextId].pId = id;
+            arr[nextId].branch = 0;
+            q_enqueue(q, curr->no, nextId);
+        }
+    }
+    
+    q_free(q); //dont need q anymore (BFS done)
+    free(q);
+
+    if (found1 == -1 || found2 == -1) {
+        free(arr);
+        mvprintw(10, 2, "Error: No such solutions.");
+        refresh();
+        return;
+    }
+
+    //now lets build ancestor array
+
+    PathNode* a1 = malloc(nodeCount * sizeof(PathNode));
+    if (a1 == NULL) {
+        free(a1);
+        free(arr);
+        mvprintw(10, 2, "Error: Memory issue.");
+        refresh();
+        return;
+    }
+    PathNode* a2 = malloc(nodeCount * sizeof(PathNode));
+    if (a2 == NULL) {
+        free(a1);
+        free(a2);
+        free(arr);
+        mvprintw(10, 2, "Error: Memory issue.");
+        refresh();
+        return;
+    }
+
+    int32_t l1 = 0;
+    int32_t l2 = 0;
+
+    int32_t currId = found1;
+    while (currId != -1) { //reached root
+        a1[l1] = arr[currId];
+        l1++;
+        currId = arr[currId].pId;
+    }
+
+    currId = found2;
+    while (currId != -1) { //reached root
+        a2[l2] = arr[currId];
+        l2++;
+        currId = arr[currId].pId;
+    }
+
+    l1--; //addr of root node
+    l2--;
+    PathNode* lca = NULL;
+
+    while (l1 >= 0 && l2 >= 0 && a1[l1].node == a2[l2].node) {
+        lca = &a1[l1]; //goes first so lca doesn't need to be changed after
+        l1--;
+        l2--;
+    }
+
+    //JUST GOTTA FIGURE OUT PRINTING NOW
+
+    //mvprintw(10, 2, "find_shortest_path not yet implemented.");
+    //refresh();
 }
